f84151edf7dfd042105d151e2409d3db11acbaad
Daniel Ayala <dmayala001@gmail.com>
2015-06-06 21:17:26 -0700

    Add paging functions to store

--- client/src/js/stores/WineStore.js
+++ client/src/js/stores/WineStore.js
@@ -2,15 +2,27 @@ import AppDispatcher from '../dispatcher/AppDispatcher'
 import {EventEmitter} from 'events';
 import Constants from '../constants/Constants';
 
+const CHANGE_EVENT = 'change';
+const PER_PAGE = 8;
+
 let _wines = [];
 let _wine = {};
+let currPage = 1;
 
 let receiveWines = (wines) =>  _wines = wines; 
 let receiveWine = (wine) => _wine = wine;
+let setCurrentPage = (pageNo) => currPage = pageNo;
 
 let WineStore = Object.assign({}, EventEmitter.prototype, {
   getWines() { return _wines; },
   getWine() { return _wine; },
+  getPages() { 
+    if (_wines.length > 0) {
+      return Math.ceil(_wines.length/PER_PAGE);
+    }
+    return 0;
+  },
+  getCurrentPage() { return currPage; },
   emitChange() { this.emit(CHANGE_EVENT); },
   addChangeListener(cb) { this.on(CHANGE_EVENT, cb); },
   removeChangeListener(cb) { this.removeListener(CHANGE_EVENT, cb); }
@@ -25,6 +37,9 @@ AppDispatcher.register((payload) => {
       break;
     case Constants.RECEIVE_WINE:
       receiveWine(action.data);
+      break;
+    case Constants.SET_CURRENT_PAGE:
+      setCurrentPage(action.id);
       break;
     default:
       return true;
