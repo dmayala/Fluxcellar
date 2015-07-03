b021fa9c93a6e69dbfae9f8f55940cb0be9b19ca
Daniel Ayala <dmayala001@gmail.com>
2015-07-02 18:18:45 -0700

    Add more CRUD util functions

--- client/src/js/utils/APIUtils.js
+++ client/src/js/utils/APIUtils.js
@@ -5,17 +5,16 @@ import ServerActions from '../actions/ServerActions';
 const endpoint = 'api/wines';
 
 export default {
-  
+
   async loadWine(id) {
     try {
       let res = await fetch(`${endpoint}/${id}`);
       let wine = await res.json();
       ServerActions.receiveWine(wine);
-    }
-    catch (err) {
+    } catch (err) {
       throw new Error(err);
     }
-  }
+  },
 
   async loadWines() {
     try {
@@ -25,5 +24,57 @@ export default {
     } catch (err) {
       throw new Error(err);
     }
+  },
+
+  async addWine(wine, image) {
+    try {
+      let form = new FormData();
+      Object.keys(wine).forEach((key) => {
+        form.append(key, wine[key]);
+      });
+
+      if (image) {
+        form.append('file', image);
+      }
+
+      let res = await fetch(endpoint, { 
+        method: 'post', 
+        body: form 
+      });
+
+      let data = await res.json();
+      return data;
+    } catch (err) {
+      throw new Error(err);
+    }
+  },
+
+  async updateWine(id, wine, image) {
+    try {
+      let form = new FormData();
+      Object.keys(wine).forEach((key) => {
+        form.append(key, wine[key]);
+      });
+
+      if (image) {
+        form.append('file', image);
+      }
+
+      let res = await fetch(`${endpoint}/${id}`, { 
+        method: 'put', 
+        body: form 
+      });
+
+      let data = await res.json();
+      return data
+    } catch (err) {
+      throw new Error(err);
+    }
+  },
+
+  async removeWine(id) {
+      let res = await fetch(`${endpoint}/${id}`, { method: 'delete' });
+      let data = await res.json();
+      return data;
   }
 }
