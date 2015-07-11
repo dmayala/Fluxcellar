90340507fc9c87a2a018c8d0835267f32abb5d17
Daniel Ayala <dmayala001@gmail.com>
2015-07-11 08:28:16 -0700

    Change WineStore to Alt style

--- client/src/js/stores/WineStore.js
+++ client/src/js/stores/WineStore.js
@@ -1,52 +1,40 @@
-import AppDispatcher from '../dispatcher/AppDispatcher'
-import {EventEmitter} from 'events';
-import Constants from '../constants/Constants';
-
 const CHANGE_EVENT = 'change';
 const PER_PAGE = 8;
 
-let _wines = [];
-let _wine = {};
-let currPage = 1;
+class WineStore {
 
-let receiveWines = (wines) =>  _wines = wines; 
-let receiveWine = (wine) => _wine = wine;
-let setCurrentPage = (pageNo) => currPage = pageNo;
-
-let WineStore = Object.assign({}, EventEmitter.prototype, {
-  getWines() { return _wines; },
-  getWine() { return _wine; },
-  getPages() { 
-    if (_wines.length > 0) {
-      return Math.ceil(_wines.length/PER_PAGE);
-    }
-    return 0;
-  },
-  getCurrentPage() { return currPage; },
-  emitChange() { this.emit(CHANGE_EVENT); },
-  addChangeListener(cb) { this.on(CHANGE_EVENT, cb); },
-  removeChangeListener(cb) { this.removeListener(CHANGE_EVENT, cb); }
-});
-
-AppDispatcher.register((payload) => {
-  let action = payload.action;
-
-  switch(action.actionType) {
-    case Constants.RECEIVE_ALL_WINES:
-      receiveWines(action.data);
-      break;
-    case Constants.RECEIVE_WINE:
-      receiveWine(action.data);
-      break;
-    case Constants.SET_CURRENT_PAGE:
-      setCurrentPage(action.id);
-      break;
-    default:
-      return true;
+  constructor() {
+    this.wines = [];
+    this.wine = {};
+    this.currentPage = 1;
+    this.pages = 1;
+    this.bindActions(this.alt.getActions('wines'));
+  }
+  
+  loadWines(wines) {
+    this.wines = wines;
+    this.pages = this.getPages();
   }
 
-  WineStore.emitChange();
-  return true;
-});
+  loadWine(wine) {
+    this.wine = wine;
+  }
+
+  setCurrentPage(pageNo) {
+    this.currentPage = pageNo;
+  }
+
+  getWine() {
+    return this.wine || {};
+  }
+
+  getPages() { 
+    let wines = this.wines;
+    if (wines.length > 0) {
+      return Math.ceil(wines.length/PER_PAGE);
+    }
+    return 0;
+  }
+}
 
 export default WineStore;
