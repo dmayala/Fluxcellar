757b4eb350dbbaa3be5cc0fe2fcdcf135b2de3c5
Daniel Ayala <dmayala001@gmail.com>
2015-07-12 09:25:00 -0700

    Pass flux instance into the router

--- client/src/js/app.js
+++ client/src/js/app.js
@@ -1,9 +1,34 @@
 import React from 'react';
-import AppRouter from './router/AppRouter';
+import Iso from 'iso';
+import Router from 'react-router';
 
-AppRouter.run((Handler) => {
-  React.render(
-    <Handler />, 
-    document.getElementById('fluxcellar')
+import routes from './router/routes';
+import Flux from './utils/flux';
+
+const bootstrap = () => {
+  return new Promise((resolve) => {
+    Iso.bootstrap((initialState, __, container) => {
+      resolve({ initialState, __, container });
+    });
+  });
+};
+
+(async () => {
+  // Initialize alt instance
+  const flux = new Flux();
+
+  // bootstrap application with data from server
+  const boot= await bootstrap();
+  flux.bootstrap(boot.initialState);
+
+  Router.run(
+    routes,
+    Router.HistoryLocation,
+    (Handler) => {
+      React.render(
+        <Handler flux={flux} />, 
+        document.getElementById('fluxcellar')
+      );
+    }
   );
-});
+})();
