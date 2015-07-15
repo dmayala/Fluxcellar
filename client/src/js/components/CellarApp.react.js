25217bbac02659d7c374d7b1dfe9a2f96d98788d
Daniel Ayala <dmayala001@gmail.com>
2015-07-14 19:35:17 -0700

    Change CellarApp to pass alt instance

--- client/src/js/components/CellarApp.react.js
+++ client/src/js/components/CellarApp.react.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import {Header} from './Header.react';
+import {Footer} from './Footer.react';
 import {RouteHandler} from 'react-router';
 
 export class CellarApp extends React.Component {
   render() {
+    const props = Object.assign({}, this.state, this.props);
     return (
       <div>
-        <Header />
-        <RouteHandler />
+        <Header {...props}/>
+        <RouteHandler {...props}/>
+        <Footer />
       </div>
     );
   }
